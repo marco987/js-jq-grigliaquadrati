9b2d259a28f946ca0755a027eedbd0583900492f
marco987 <pacemarco87@gmail.com>
2019-07-17 17:40:31 +0200

    Push delle 17:40

--- script.js
+++ script.js
@@ -2,3 +2,21 @@
 // tutti bianchi. 15 di questi quadratini
 // (scelti a caso all’inizio) se cliccati
 // diventano rossi, gli altri diventano verdi.
+
+// $(".red").click(function() {
+//   $(this).css("background-color", "red");
+// })
+
+$(".quad").click(function() {
+  if ($("div").hasClass("red")) {
+    $(this).css("background-color", "red");
+  } else {
+    $(this).css("background-color", "green")
+  }
+})
+
+console.log($("div").attr("class") == "quad red");
+
+console.log($("div").hasClass("red"));
+
+console.log($("div").attr("class"));
